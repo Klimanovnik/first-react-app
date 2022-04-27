6535acb1df3aa5db2e2f738bf7dd365c98dbea92
klimanovnik <klimanovnik98@gmail.com>
2022-04-27 15:51:53 +0300

    Refactor Users to class component

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -1,58 +1,62 @@
+import React from "react";
 import styles from './Users.module.css';
 import axios from "axios";
 import defaultUserSmallPhoto from '../../assets/img/defaultUserSmallPhoto.png';
 
-function Users(props) {
+class Users extends React.Component {
 
-    const addUsers = function () {
-        if (props.users.length === 0) {
+    constructor(props)  {
+        super(props);
+
+        if (this.props.users.length === 0) {
             axios
                 .get("https://social-network.samuraijs.com/api/1.0/users")
                 .then(response => {
-                    props.setUsers(response.data.items);
+                    this.props.setUsers(response.data.items);
                 });
         }
-    };
+    }
 
-    return (
-        <section className={styles.usersWrap}>
-            <button type="button" onClick={addUsers}>Add Users</button>
-            {
-                props.users.map(user => {
-                    return (
-                        <div key={user.id} className={styles.user}>
-                            <div>
-                                <img className={styles.userIcon}
-                                     src={
-                                         user.photos.small
-                                             ? user.photos.small
-                                             : defaultUserSmallPhoto
-                                     }
-                                     alt="No Image"
-                                />
-                                <button
-                                    type="button"
-                                    onClick={() => {
-                                        props.toggleFollow(user.id);
-                                    }}
-                                >
-                                    {user.followed ? "Отписаться" : "Подписаться"}
-                                </button>
-                            </div>
-                            <div className={styles.userDescription}>
-                                <div className={styles.fullName}>
-                                    {user.name}
+    render() {
+        return (
+            <section className={styles.usersWrap}>
+                {
+                    this.props.users.map(user => {
+                        return (
+                            <div key={user.id} className={styles.user}>
+                                <div>
+                                    <img className={styles.userIcon}
+                                         src={
+                                             user.photos.small
+                                                 ? user.photos.small
+                                                 : defaultUserSmallPhoto
+                                         }
+                                         alt="No Image"
+                                    />
+                                    <button
+                                        type="button"
+                                        onClick={() => {
+                                            this.props.toggleFollow(user.id);
+                                        }}
+                                    >
+                                        {user.followed ? "Отписаться" : "Подписаться"}
+                                    </button>
                                 </div>
-                                <div className={styles.status}>
-                                    {user.status}
+                                <div className={styles.userDescription}>
+                                    <div className={styles.fullName}>
+                                        {user.name}
+                                    </div>
+                                    <div className={styles.status}>
+                                        {user.status}
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                    );
-                })
-            }
-        </section>
-    );
+                        );
+                    })
+                }
+            </section>
+        );
+    }
 }
 
 export default Users;
