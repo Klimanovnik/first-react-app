4a18a01ef8f2c4bb28037c80486141e19cdf48cf
Nikita Klimanov <n.klimanov@elfsight.com>
2022-05-24 09:30:13 +0300

    Add server follow-unfollow

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -2,6 +2,7 @@ import styles from './Users.module.css';
 import defaultUserPhoto from '../../assets/img/defaultUserPhoto.png';
 import Preloader from "../Common/Preloader/Preloader";
 import {NavLink} from "react-router-dom";
+import axios from "axios";
 
 const Users = function (props) {
     return (
@@ -51,6 +52,9 @@ const Users = function (props) {
                                                  alt="No Image"
                                             />
                                             <div className={styles.userDescription}>
+                                                <div className={styles.id}>
+                                                    {user.id}
+                                                </div>
                                                 <div className={styles.fullName}>
                                                     {user.name}
                                                 </div>
@@ -63,9 +67,45 @@ const Users = function (props) {
                                         <button
                                             type="button"
                                             onClick={() => {
-                                                props.toggleFollow(user.id);
+                                                const requestType = user.followed ? "delete" : "post";
+
+                                                const requestOptions = {
+                                                    withCredentials: true,
+                                                    headers: {
+                                                        "API-KEY": "c00d2ff4-69c2-49cb-bfec-cd6ef83bd398"
+                                                    }
+                                                };
+
+                                                let optionsForDelete = {};
+                                                let optionsForPost = {};
+
+                                                if (requestType === "delete") {
+                                                    optionsForDelete = {
+                                                        ...requestOptions
+                                                    };
+                                                } else {
+                                                    optionsForPost = {
+                                                        ...requestOptions
+                                                    };
+                                                }
+
+                                                axios
+                                                    [requestType](`https://social-network.samuraijs.com/api/1.0/follow/${user.id}`,
+                                                    {
+                                                        ...optionsForDelete
+                                                    },
+                                                    {
+                                                        ...optionsForPost
+                                                    }
+                                                )
+                                                    .then(response => {
+                                                        if (response.data.resultCode === 0) {
+                                                            props.toggleFollow(user.id);
+                                                        }
+                                                    });
                                             }}
                                             className={styles.userButton}
+                                            disabled={!props.isAuth}
                                         >
                                             {user.followed ? "Отписаться" : "Подписаться"}
                                         </button>
--- src/components/Users/UsersContainer.js
+++ src/components/Users/UsersContainer.js
@@ -14,16 +14,17 @@ const mapStateToProps = function (state) {
     return {
         users: state.usersPage.users,
         pagination: state.usersPage.pagination,
-        isFetching: state.usersPage.isFetching
+        isFetching: state.usersPage.isFetching,
+        isAuth: state.auth.isAuth
     };
 };
 
 const mapDispatchToProps = {
-        toggleFollow,
-        setUsers,
-        setCurrentPage,
-        setUsersTotalCount,
-        toggleFetching
+    toggleFollow,
+    setUsers,
+    setCurrentPage,
+    setUsersTotalCount,
+    toggleFetching
 };
 
 class UsersServerAPI extends React.Component {
@@ -32,7 +33,11 @@ class UsersServerAPI extends React.Component {
             this.props.toggleFetching(true);
 
             axios
-                .get(`https://social-network.samuraijs.com/api/1.0/users?count=${this.props.pagination.countPerRequest}&page=${this.props.pagination.currentPage}`)
+                .get(`https://social-network.samuraijs.com/api/1.0/users?count=${this.props.pagination.countPerRequest}&page=${this.props.pagination.currentPage}`,
+                    {
+                        withCredentials: true
+                    }
+                )
                 .then(response => {
                     //this.props.setUsersTotalCount(response.data.totalCount);
                     this.props.toggleFetching(false);
@@ -47,7 +52,11 @@ class UsersServerAPI extends React.Component {
         this.props.toggleFetching(true);
 
         axios
-            .get(`https://social-network.samuraijs.com/api/1.0/users?count=${this.props.pagination.countPerRequest}&page=${newPage}`)
+            .get(`https://social-network.samuraijs.com/api/1.0/users?count=${this.props.pagination.countPerRequest}&page=${newPage}`,
+                {
+                    withCredentials: true
+                }
+            )
             .then(response => {
                 this.props.toggleFetching(false);
                 this.props.setUsers(response.data.items);
@@ -57,11 +66,8 @@ class UsersServerAPI extends React.Component {
     render() {
         return (
             <Users
-                pagination={this.props.pagination}
+                {...this.props}
                 onPageChanged={this.onPageChanged}
-                users={this.props.users}
-                toggleFollow={this.props.toggleFollow}
-                isFetching={this.props.isFetching}
             />
         );
     }
